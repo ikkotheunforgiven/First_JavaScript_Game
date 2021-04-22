e826ecad4dda3957d16cddb5f43549c11e930107
ikkotheunforgiven <82880424+ikkotheunforgiven@users.noreply.github.com>
2021-04-22 21:59:19 +0200

    Add files via upload

---
+++ Juego de prueba/js/player.js
@@ -0,0 +1,103 @@
+let player;
+
+function Player(classType, health, mana, strength, agility, speed) {
+    this.classType = classType;
+    this.health = health;
+    this.mana = mana;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
+}
+
+let PlayerMoves = {
+    calcAttack: function() {
+        // ¿Quién ataca primero?
+        let getPlayerSpeed = player.speed;
+        let getEnemySpeed = enemy.speed;
+        // Jugador ataca
+    let playerAttack = function() {
+        let calcBaseDamage;
+        if (player.mana > 0) {
+            calcBaseDamage = player.strength * player.mana / 1000;
+        } else {
+            calcBaseDamage = player.strength * player.agility / 1000;
+        }
+        let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+        let calcOutputDamage = calcBaseDamage + offsetDamage;
+        // Número de golpes
+        let numberOfHits = Math.floor(Math.random() * Math.floor(player.agility / 10) / 2) + 1;
+        let attackValues = [calcOutputDamage, numberOfHits];
+        return attackValues;
+    }
+    // Enemigo ataca
+    let enemyAttack = function() {
+        let calcBaseDamage;
+        if (enemy.mana > 0) {
+            calcBaseDamage = enemy.strength * enemy.mana / 1000;
+        } else {
+            calcBaseDamage = enemy.strength * enemy.agility / 1000;
+        }
+        let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+        let calcOutputDamage = calcBaseDamage + offsetDamage;
+        // Número de golpes
+        let numberOfHits = Math.floor(Math.random() * Math.floor(enemy.agility / 10) / 2) + 1;
+        let attackValues = [calcOutputDamage, numberOfHits];
+        return attackValues;
+    }
+    // Obtener vida de jugador/enemigo para cambiar luego
+    let getPlayerHealth = document.querySelector(".health-player");
+    let getEnemyHealth = document.querySelector(".health-enemy");
+    // Iniciar ataques
+    if (getPlayerSpeed >= getEnemySpeed) {
+        let playerAttackValues = playerAttack();
+        let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+        enemy.health = enemy.health - totalDamage;
+        alert("Has hecho "+ playerAttackValues[0] +" de daño, atacando "+ playerAttackValues[1]+" veces.");
+        if (enemy.health <= 0) {
+            alert("¡Has ganado! Refresca el navegador para jugar otra vez");
+            getPlayerHealth.innerHTML = 'Health: '+ player.health;
+            getEnemyHealth.innerHTML = 'Health: 0';
+        } else {
+            getEnemyHealth.innerHTML = 'Health: '+ enemy.health;
+            // Enemigo ataca
+            let enemyAttackValues = enemyAttack();
+            let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+            player.health = player.health - totalDamage;
+            alert("El enemigo ha hecho "+ enemyAttackValues[0] +" de daño, atacando "+ enemyAttackValues[1]+" veces.");
+            if (player.health <= 0) {
+                alert("Has perdido... Refresca el navegador para jugar otra vez");
+                getPlayerHealth.innerHTML = 'Health: 0';
+                getEnemyHealth.innerHTML = 'Health: '+ enemy.health;
+            } else {
+                getPlayerHealth.innerHTML = 'Health: '+ player.health;
+            }
+        }
+    } else if (getEnemySpeed >= getPlayerSpeed) {
+        // Enemigo ataca
+        let enemyAttackValues = enemyAttack();
+        let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+        player.health = player.health - totalDamage;
+        alert("El enemigo ha hecho "+ enemyAttackValues[0] +" de daño, atacando "+ enemyAttackValues[1]+" veces.");
+        if (player.health <= 0) {
+            alert("Has perdido... Refresca el navegador para jugar otra vez");
+            getEnemyHealth.innerHTML = 'Health: '+ enemy.health;
+            getPlayerHealth.innerHTML = 'Health: 0';
+        } else {
+            getPlayerHealth.innerHTML = 'Health: '+ player.health;
+            // Jugador ataca
+            let playerAttackValues = playerAttack();
+            let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+            enemy.health = enemy.health - totalDamage;
+            alert("Has hecho "+ playerAttackValues[0] +" de daño, atacando "+ playerAttackValues[1]+" veces.");
+            if (enemy.health <= 0) {
+                alert("¡Has ganado! Refresca el navegador para jugar otra vez");
+                getEnemyHealth.innerHTML = 'Health: 0';
+                getPlayerHealth.innerHTML = 'Health: '+ player.health;
+            } else {
+                getEnemyHealth.innerHTML = 'Health: '+ enemy.health;
+            }
+        }
+    }
+    }
+    
+}
